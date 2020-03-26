5bcf4590d1422930b8ed2a038e55994bbd563edf
gwalker541 <gwalker1@email.arizona.edu>
2020-03-25 18:56:10 -0700

    added paper

--- Code/hello.f95
+++ Code/hello.f95
@@ -1,9 +1,4 @@
 program hello
 	implicit none
-    
- 	character (len = 15) :: word
 	print *, "Hello, World!"
-	print *, "Enter a word for me to say:"
-        read *, word
-	print *, word
 end program hello
